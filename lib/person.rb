9eb01d25d7fb29da336824989ffe4d975fc73cc5
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-17 22:58:46 +0300

    DOne

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
+    attr_accessor :name
+    attr_accessor :job
+  end
 
-end
+person = Person.new
+person.name = "Beyonce"
+puts person.name
+
+person_ = Person.new
+person.job = "Singer"
+puts person.job
