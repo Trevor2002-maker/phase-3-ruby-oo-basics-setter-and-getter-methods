dabeb33f79977c986567016f6a575a581f09c67f
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-17 22:58:35 +0300

    commit tree 4993cde36001170c485f84b943d8288595ed2e38

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-
+  attr_accessor :name
+  attr_accessor :breed
 end
+
+my_instance = Dog.new
+my_instance.name = "Fido"
+puts my_instance.name 
+
+my_dog = Dog.new
+my_dog.breed = "Beagle"
+puts my_dog.breed 
